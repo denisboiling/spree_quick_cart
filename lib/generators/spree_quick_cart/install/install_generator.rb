07d1b2b44c4653dd93900577eddbcd05b9e216c6
Denis Gabdrakhmanov <denisboiling@gmail.com>
2014-03-20 18:35:07 +0300

    fix path

--- lib/generators/spree_quick_cart/install/install_generator.rb
+++ lib/generators/spree_quick_cart/install/install_generator.rb
@@ -5,12 +5,12 @@ module SpreeQuickCart
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_quick_cart\n"
+        append_file 'app/assets/javascripts/spree/frontend/all.js', "//= require spree_quick_cart\n"
         # append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_quick_cart\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_quick_cart\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/assets/stylesheets/spree/frontend/all.css', " *= require spree_quick_cart\n", :before => /\*\//, :verbose => true
         # inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_quick_cart\n", :before => /\*\//, :verbose => true
       end
 
